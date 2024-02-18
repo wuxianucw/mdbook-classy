e7447b4f0bd3864e14eeaf123e9bafe88601b80f
无限UCW <x-wuxian@qq.com>
2024-02-19 02:05:23 +0800

    fix preprocessor
    should fix: Reviewable/mdbook-classy#2, Reviewable/mdbook-classy#3, Reviewable/mdbook-classy#10, Reviewable/mdbook-classy#15

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use clap::{arg, command, ArgMatches, Command};
 use mdbook::book::{Book, Chapter};
 use mdbook::errors::Error;
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
-use pulldown_cmark::{CowStr, Event, Parser, Tag};
+use mdbook::utils::new_cmark_parser;
+use pulldown_cmark::{CowStr, Event, Tag};
 use std::io;
 use std::process;
 
@@ -34,92 +35,81 @@ impl Preprocessor for Classy {
     }
 }
 
-#[derive(Debug)]
-struct ClassAnnotation {
-    pub class: String,
-    pub index: usize,
-    pub paragraph_start: usize,
-    pub paragraph_end: Option<usize>,
+#[derive(Debug, Clone, Copy)]
+enum State<'a> {
+    BeforeStart,
+    Expecting,
+    Accepted(&'a str),
+    Inner,
 }
 
 /// This is where the markdown transformation actually happens.
 /// Take paragraphs beginning with `{:.class-name}` and give them special rendering.
 /// Mutation: the payload here is that it edits chapter.content.
 fn classy(chapter: &mut Chapter) -> Result<(), Error> {
-    // 1. Parse the inbound markdown into an Event vector.
-    let incoming_events: Vec<Event> = Parser::new(&chapter.content).collect();
+    let parser = new_cmark_parser(&chapter.content, false);
 
-    // 2. Find paragraphs beginning with the class annotator `{:.class-name}` and record their information in
-    // a vector of ClassAnnotation structs.
-    let mut class_annotations: Vec<ClassAnnotation> = vec![];
-    for i in 0..incoming_events.len() {
-        let event = &incoming_events[i];
-        match *event {
-            Event::Text(CowStr::Borrowed(text)) => {
-                if i > 0 {
-                    if let Event::Start(Tag::Paragraph) = incoming_events[i - 1] {
-                        let v: Vec<_> = text.split("").collect();
-                        let len_v = v.len();
-                        if v[..4].join("") == "{:." && v[(len_v - 2)..].join("") == "}" {
-                            let class = v[4..(len_v - 2)].join("");
-                            class_annotations.push(ClassAnnotation {
-                                class,
-                                index: i,
-                                paragraph_start: i - 1,
-                                paragraph_end: None,
-                            })
-                        }
-                    }
+    let mut state = State::BeforeStart;
+    let mut new_events = vec![];
+
+    for event in parser {
+        if let State::Accepted(text) = state {
+            if matches!(event, Event::SoftBreak | Event::HardBreak) {
+                state = State::Inner;
+                new_events.push(Event::Start(Tag::Paragraph));
+            } else {
+                state = State::BeforeStart;
+                new_events.pop().unwrap();
+                new_events.push(Event::Start(Tag::Paragraph));
+                new_events.push(Event::Text(text.into()));
+                new_events.push(event);
+            }
+            continue;
+        }
+
+        match event {
+            Event::Start(Tag::Paragraph) => {
+                state = State::Expecting;
+            }
+
+            Event::Text(CowStr::Borrowed(text)) if matches!(state, State::Expecting) => {
+                // "{:.class}", "{:.class1 class2}", ...
+                // event sequence: Start(Paragraph) -> Text(Borrowed(_)) -> SoftBreak | HardBreak
+                if text.len() > "{:.}".len() && text.starts_with("{:.") && text.ends_with('}') {
+                    state = State::Accepted(text);
+                    new_events.push(Event::Html(
+                        format!("<div class=\"{}\">\n", &text[3..text.len() - 1]).into(),
+                    ));
+                } else {
+                    state = State::BeforeStart;
+                    new_events.push(Event::Start(Tag::Paragraph));
+                    new_events.push(Event::Text(text.into()));
                 }
             }
+
             Event::End(Tag::Paragraph) => {
-                let last = class_annotations.last_mut();
-                if let Some(class_command) = last {
-                    if class_command.paragraph_end.is_none() {
-                        class_command.paragraph_end = Some(i);
-                    }
+                if matches!(state, State::Expecting) {
+                    new_events.push(Event::Start(Tag::Paragraph));
                 }
+                new_events.push(Event::End(Tag::Paragraph));
+                if matches!(state, State::Inner) {
+                    new_events.push(Event::Html("</div>\n".into()));
+                }
+                state = State::BeforeStart;
             }
-            _ => {}
+
+            ev => {
+                if matches!(state, State::Expecting) {
+                    state = State::BeforeStart;
+                    new_events.push(Event::Start(Tag::Paragraph));
+                }
+                new_events.push(ev);
+            }
         }
     }
 
-    // 3. Construct a new_events vector with <div class="class-name">\n \n</div> around the annotated paragraphs
-    // (and with the class annotation removed).
-    let mut slices = vec![];
-    let mut last_end = 0;
-    let div_starts: Vec<Event> = class_annotations
-        .iter()
-        .map(|ca| Event::Html(CowStr::from(format!("<div class=\"{}\">", ca.class))))
-        .collect();
-    let div_end: Vec<Event> = vec![Event::Html(CowStr::from("</div>"))];
-    for (i, ca) in class_annotations.iter().enumerate() {
-        // Add unclassed events.
-        slices.push(&incoming_events[last_end..ca.paragraph_start]);
-
-        last_end = ca.paragraph_end.unwrap_or(incoming_events.len() - 1);
-
-        let paragraph = &incoming_events[ca.paragraph_start..(last_end + 1)];
-
-        // Add <div class="class-name">
-        slices.push(&div_starts[i..i + 1]);
-
-        // Add paragraph opener.
-        slices.push(&paragraph[0..1]);
-
-        // Add the rest of the paragraph, skipping the class annotation.
-        slices.push(&paragraph[2..]);
-
-        // Add </div>.
-        slices.push(&div_end[..]);
-    }
-    slices.push(&incoming_events[last_end..]);
-    let new_events = slices.concat();
-
-    // 4. Update chapter.content using markdown generated from the new event vector.
-    let mut buf = String::with_capacity(chapter.content.len() + 128);
-    pulldown_cmark_to_cmark::cmark(new_events.into_iter(), &mut buf)
-        .expect("can re-render cmark");
+    let mut buf = String::with_capacity(chapter.content.capacity());
+    pulldown_cmark_to_cmark::cmark(new_events.into_iter(), &mut buf).expect("can re-render cmark");
     chapter.content = buf;
     Ok(())
 }
@@ -150,8 +140,10 @@ fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
 
 /// Check to see if we support the processor (classy only supports html right now)
 fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
-    let renderer = sub_args.get_one::<String>("renderer").expect("Required argument");
-    let supported = pre.supports_renderer(&renderer);
+    let renderer = sub_args
+        .get_one::<String>("renderer")
+        .expect("Required argument");
+    let supported = pre.supports_renderer(renderer);
 
     if supported {
         process::exit(0);
